1033e16dd3b8770f0ce72fc5e857380edebe1cff
Sinon <34333654+Catta1997@users.noreply.github.com>
2026-03-08 11:29:09 +0100

    feat: new options

--- options.js
+++ options.js
@@ -4,17 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const deletedCheckbox   = document.getElementById('toggleDeleted');
     const highlightModerators   = document.getElementById('toggleHighlightModerator');
     const highlightMentions   = document.getElementById('toggleHighlightMentions');
+    const highlightNewUserCheckbox   = document.getElementById('togglehighlightNewUser');
+
     // Carica le impostazioni salvate (default: true)
     chrome.storage.sync.get({
         highlightEnabled: true,
         highlightMods: true,
         highlightMentions: true,
-        deletedEnabled: true
+        deletedEnabled: true,
+        newUserEnabled: true,
     }, prefs => {
         highlightCheckbox.checked = prefs.highlightEnabled;
         highlightMentions.checked = prefs.highlightMentions;
         deletedCheckbox.checked   = prefs.deletedEnabled;
         highlightModerators.checked = prefs.highlightMods;
+        highlightNewUserCheckbox.checked = prefs.newUserEnabled;
     });
 
     // Al cambiamento salva su storage.sync
@@ -29,5 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     deletedCheckbox.addEventListener('change', () => {
         chrome.storage.sync.set({ deletedEnabled: deletedCheckbox.checked });
+    });
+    highlightNewUserCheckbox.addEventListener('change', () => {
+        chrome.storage.sync.set({ newUserEnabled: highlightNewUserCheckbox.checked });
     });
 });
